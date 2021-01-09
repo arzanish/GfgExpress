8b6afd71f0c7d23daa4b19b603995f7eb39f7215
Arzanish Yusuf Nadeem <arzanish.y.n@outlook.com>
2021-01-09 23:27:07 +0530

    created a basic ExpressJS webserver

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello Geeks");
+const express = require("express"); // similar to import in java
+const app = express(); // considering express as a class , so making an object
+const port = 5000; // choosing a port
+
+app.get("/", (req, res) => {
+  res.send("Hello Geeksforgeeks");
+});
+
+app.listen(port, () => {
+  console.log("Using port" + port + ".");
+}); // establishing connection to the port
